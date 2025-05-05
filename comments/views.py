58887afca87b5b2a345aee80345a73ea480b3b38
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-05-05 20:53:24 +0500

    refresh view

--- comments/views.py
+++ comments/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render, redirect
 
 from comments.models import Comments
 from posts.models import Posts
+from clients.models import Client
 
 
 logger = logging.getLogger()
@@ -16,7 +17,7 @@ logger = logging.getLogger()
 class AddComment(View):
     def post(self, request: HttpRequest, pk: int) -> HttpResponse:
         client = request.user
-        if not client:
+        if not isinstance(client, Client):
             return JsonResponse(data={"error": "not authorized"})
         posts = Posts.objects.filter(id=pk)
         if not posts:
@@ -28,12 +29,25 @@ class AddComment(View):
             text=request.POST.get("text")
         )
         comment.save()
-        return render(
-            request=request, template_name="pk_post.html",
-            context={"post": posts[0], "author": client}
-        )
+        return redirect("pk_post", pk=pk)
 
 
 class AddReply(View):
-    def post(self):
-        pass
+    def post(self, request: HttpRequest, pk: int) -> HttpResponse:
+        client = request.user
+        if not isinstance(client, Client):
+            return JsonResponse(data={"error": "not authorized"})
+        comments = Comments.objects.filter(id=pk)
+        if not comments:
+            return JsonResponse(
+                data={"error": f"Comment with id {pk} not found"}
+            )
+        comment = Comments(
+            post=comments[0].post,
+            user=client,
+            parent=comments[0],
+            text=request.POST.get("text")
+        )
+        comment.save()
+        return redirect("pk_post", pk=comments[0].post.pk)
+    
